c5d883d51ab4acb17a6280d9954c3b4bbb06d4b4
Leandro Calcabrini <leacalca@gmail.com>
2024-05-20 14:16:09 -0300

    carga de codigo

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Question } from './assets/Questions'
+import { Card } from './assets/Card'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+  <Card>
+    
+  </Card>
+
+  </>
+)
